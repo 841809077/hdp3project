e6a0f24654cc17559f32af3c8aa7257fd3a443ff
e <liuyongzhi95@163.com>
2019-06-19 15:24:31 +0800

    增加生产者拦截器

--- src/main/java/com/hdp/project/hdpproject/kafka/KafkaProducerAnalysis.java
+++ src/main/java/com/hdp/project/hdpproject/kafka/KafkaProducerAnalysis.java
@@ -9,44 +9,55 @@ import java.util.concurrent.Future;
 
 /**
  * @author Liuyongzhi
- * @description:
- * @date 2019/6/18 0018
+ * @description: 生产者发送消息的三种方式
+ * @date 2019/6/18
  */
 public class KafkaProducerAnalysis {
 
-    private static final String brokerList = "node71.xdata:6667,node72.xdata:6667,node73.xdata:6667";
-    private static final String topic = "test";
+    private static final String BROKERLIST = "node71.xdata:6667,node72.xdata:6667,node73.xdata:6667";
+    private static final String TOPIC = "test";
 
     private static Properties initConfig() {
         Properties props = new Properties();
-        props.put("bootstrap.servers", brokerList);
+        props.put("bootstrap.servers", BROKERLIST);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("client.id", "producer.client.id.demo");
         props.put("retries", 3);
         // acks有三个匹配项，均为字符串类型，分别为："1"，"0","all或-1"
         props.put(ProducerConfig.ACKS_CONFIG, "1");
+        // 指定拦截器
+        props.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, ProducerInterceptorPrefix.class.getName());
         return props;
     }
 
-    // 方式一：发后即忘，性能高，可靠性差，易发生信息丢失。
-    private static void fire_and_forget_send(Properties props) {
+    /**
+     * @description: 方式一：发后即忘，性能高，可靠性差，易发生信息丢失。
+     * @param: props
+     * @return: void
+     */
+    private static void fireAndForgetSend(Properties props) {
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "fire and forget send!");
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "fire and forget send!");
         producer.send(record);
         producer.close();
     }
 
-    // 方式二：同步发送消息，可靠性高，要么消息被发送成功，要么发生异常。如果发生异常，可以捕获并进行相应的处理。
-    // 性能较发后即忘的方式差，需要阻塞等待一条消息发送完再发送下一条信息。
-    private static void sync_send(Properties props) {
+    /**
+     * @description: 方式二：同步发送消息，可靠性高，要么消息被发送成功，要么发生异常。如果发生异常，可以捕获并进行相应的处理。
+     * 性能较"发后即忘"的方式差，需要阻塞等待一条消息发送完再发送下一条信息。
+     * @param: props
+     * @return: void
+     */
+    private static void syncSend(Properties props) {
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "sync send!");
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "sync send!");
         try {
             // 方式一
 //            producer.send(record).get();
             // 方式二
-            Future<RecordMetadata> future = producer.send(record);  // future代表一个任务的声明周期。
+            // future代表一个任务的声明周期。
+            Future<RecordMetadata> future = producer.send(record);
             // 获取消息的元数据信息，比如当前消息的主题、分区号、分区中的偏移量（offset）、时间戳等。
             // 如果在应用代码中需要这些信息，可以使用这种方式。如果不需要，可采用方式一的写法。
             RecordMetadata metadata = future.get();
@@ -57,18 +68,19 @@ public class KafkaProducerAnalysis {
         producer.close();
     }
 
-    // 方式三：异步发送消息，增加一个回调函数。单纯的send()方法也是异步请求
-    private static void async_send(Properties props) {
+    /**
+     * @description: 方式三：异步发送消息，增加一个回调函数。单纯的send()方法也是异步请求。
+     * @param: props
+     * @return: void
+     */
+    private static void asyncSend(Properties props) {
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "async send!");
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                if (e != null) {
-                    e.printStackTrace();
-                } else {
-                    System.out.println(recordMetadata.topic() + " - " + recordMetadata.partition() + " - " + recordMetadata.offset());
-                }
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "async send!");
+        producer.send(record, (recordMetadata, e) -> {
+            if (e != null) {
+                e.printStackTrace();
+            } else {
+                System.out.println(recordMetadata.topic() + " - " + recordMetadata.partition() + " - " + recordMetadata.offset());
             }
         });
         producer.close();
@@ -76,9 +88,9 @@ public class KafkaProducerAnalysis {
 
     public static void main(String[] args) {
         Properties props = initConfig();
-//        fire_and_forget_send(props);
-//        sync_send(props);
-        async_send(props);
+//        fireAndForgetSend(props);
+//        syncSend(props);
+        asyncSend(props);
     }
 
 }
---
+++ src/main/java/com/hdp/project/hdpproject/kafka/ProducerInterceptorPrefix.java
@@ -0,0 +1,45 @@
+package com.hdp.project.hdpproject.kafka;
+
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Map;
+
+/**
+ * @author liuyzh
+ * @description: 生产者的拦截器
+ * @date 2019/6/19 13:08
+ */
+public class ProducerInterceptorPrefix implements ProducerInterceptor {
+
+    private volatile long sendSuccess = 0;
+    private volatile long sendFailure = 0;
+
+    @Override
+    public ProducerRecord onSend(ProducerRecord producerRecord) {
+        String modifiedValue = "prefix1 - " + producerRecord.value();
+        ProducerRecord<String, String> record = new ProducerRecord(producerRecord.topic(), producerRecord.partition(), producerRecord.timestamp(), producerRecord.key(), modifiedValue, producerRecord.headers());
+        return record;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata recordMetadata, Exception e) {
+        if (e == null) {
+            sendSuccess++;
+        } else {
+            sendFailure++;
+        }
+    }
+
+    @Override
+    public void close() {
+        double successRatio = (double) sendSuccess / (sendSuccess + sendFailure);
+        System.out.println("发送成功率：" + String.format("%f", successRatio * 100) + "%");
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
