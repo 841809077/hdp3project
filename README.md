fb9ad28058873a66817c527a5107e1165d1e8cd9
841809077 <liuyongzhi95@163.com>
2019-08-09 00:19:07 +0800

    完善Elasticsearch BulkProcessor的具体实现 1、实现map和json类型数据批量增加 2、根据文档id实现批量删除

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ hdp api demo
 
 2、HDP 3.0.1.0 + Kafka 1.1.1 的 api demo
 
+3、Elasticsearch 6.5.0 的 BulkProcessor 的具体实现（增删）
+
 ...
--- src/main/java/com/hdp3/project/elasticsearch/ESBulkProcessor.java
+++ src/main/java/com/hdp3/project/elasticsearch/ESBulkProcessor.java
@@ -4,6 +4,7 @@ import org.elasticsearch.action.bulk.BackoffPolicy;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -11,18 +12,23 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liuyzh
- * @description:
+ * @description: BulkProcessor的具体实现（批量增删）
  * @date 2019/8/5 16:40
  */
 public class ESBulkProcessor {
@@ -52,43 +58,121 @@ public class ESBulkProcessor {
                     @Override
                     public void beforeBulk(long executionId,
                                            BulkRequest request) {
-                        System.out.println("序号：" + executionId + "，开始执行" + request.numberOfActions() + "条数据批量插入");
+                        logger.info("序号：{} ，开始执行 {} 条数据批量操作。", executionId, request.numberOfActions());
                     }
 
                     @Override
                     public void afterBulk(long executionId,
                                           BulkRequest request,
                                           BulkResponse response) {
-                        System.out.println("序号：" + executionId + "，执行" + request.numberOfActions() + "条数据批量插入成功");
+                        // 在每次执行BulkRequest后调用，通过此方法可以获取BulkResponse是否包含错误
+                        if (response.hasFailures()) {
+                            logger.error("Bulk {} executed with failures", executionId);
+                        } else {
+                            logger.info("序号：{} ，执行 {} 条数据批量操作成功，共耗费{}毫秒。", executionId, request.numberOfActions(), response.getTook().getMillis());
+                        }
                     }
 
                     @Override
                     public void afterBulk(long executionId,
                                           BulkRequest request,
                                           Throwable failure) {
-                        System.out.println("序号：" + executionId + " 执行失败，总记录数：" + request.numberOfActions() + "，报错信息为：" + failure.getMessage());
+                        logger.error("序号：{} 批量操作失败，总记录数：{} ，报错信息为：{}", executionId, request.numberOfActions(), failure.getMessage());
                     }
                 })
+                // 每添加1000个request，执行一次bulk操作
                 .setBulkActions(1000)
+                // 每达到5M的请求size时，执行一次bulk操作
                 .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
-                .setFlushInterval(TimeValue.timeValueSeconds(20))
+                // 每5s执行一次bulk操作
+                .setFlushInterval(TimeValue.timeValueSeconds(5))
+                // 设置并发请求数。默认是1，表示允许执行1个并发请求，积累bulk requests和发送bulk是异步的，其数值表示发送bulk的并发线程数（可以为2、3、...）；若设置为0表示二者同步。
                 .setConcurrentRequests(1)
+                // 最大重试次数为3次，启动延迟为100ms。
                 .setBackoffPolicy(
                         BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))
                 .build();
     }
 
-    public static void main(String[] args) {
+    /**
+     * @return void
+     * @description: 使用bulkProcessor批量插入map数据
+     */
+    private void mapData() {
         ESBulkProcessor esBulkProcessor = new ESBulkProcessor();
         BulkProcessor esBulk = esBulkProcessor.bulkProcessor();
         Map<String, Object> m = new HashMap<>();
-        for (int i = 5000; i < 10000; i++) {
-            m.clear();
-            m.put("test2", i + "ceshi");
-            // 添加单次请求
-            esBulk.add(new IndexRequest("ceshi", "_doc", String.valueOf(i)).source(m));
+        for (int i = 0; i < 3000; i++) {
+            m.put("name", "name" + i);
+            m.put("age", new Random().nextInt(50));
+            m.put("sex", Math.round(Math.random()) == 1 ? "男" : "女");
+            esBulk.add(new IndexRequest("es_map_test", "_doc", String.valueOf(i)).source(m));
         }
+        // 最后执行一次刷新操作
         esBulk.flush();
-        esBulk.close();
+        // 30秒后关闭BulkProcessor
+        try {
+            esBulk.awaitClose(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return void
+     * @description: 使用bulkProcessor批量插入json数据
+     */
+    private void jsonData() {
+        ESBulkProcessor esBulkProcessor = new ESBulkProcessor();
+        BulkProcessor esBulk = esBulkProcessor.bulkProcessor();
+        for (int i = 0; i < 3000; i++) {
+            try {
+                XContentBuilder builder = XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("name", "name" + i)
+                        .field("age", new Random().nextInt(50))
+                        .field("sex", Math.round(Math.random()) == 1 ? "男" : "女")
+                        .endObject();
+                // 如果json是String类型的话，xx.source(jsonString, XContentType.JSON)
+                esBulk.add(new IndexRequest("es_json_test", "_doc", String.valueOf(i)).source(builder));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // 最后执行一次刷新操作
+        esBulk.flush();
+        // 30秒后关闭BulkProcessor
+        try {
+            esBulk.awaitClose(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return void
+     * @description: 使用bulkProcessor批量数据
+     */
+    private void bulkDelete() {
+        ESBulkProcessor esBulkProcessor = new ESBulkProcessor();
+        BulkProcessor esBulk = esBulkProcessor.bulkProcessor();
+        for (int i = 0; i < 3000; i++) {
+            esBulk.add(new DeleteRequest("es_map_test", "_doc", String.valueOf(i)));
+        }
+        // 最后执行一次刷新操作
+        esBulk.flush();
+        // 30秒后关闭BulkProcessor
+        try {
+            esBulk.awaitClose(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ESBulkProcessor esBulkProcessor = new ESBulkProcessor();
+//        esBulkProcessor.mapData();
+//        esBulkProcessor.jsonData();
+        esBulkProcessor.bulkDelete();
     }
 }
